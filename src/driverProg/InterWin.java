67a40f9a87e38868007e35f43b1a1f49f3c0d6a3
Shahab Abbasi Rad <darkoob12@gmail.com>
2013-01-08 01:56:40 +0330

    trying to add an option panel for chabging algorithms parameters.

--- src/driverProg/InterWin.java
+++ src/driverProg/InterWin.java
@@ -1,23 +1,36 @@
 package driverProg;
 
 import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.SwingConstants;
-import javax.swing.JButton;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.JTextArea;
-import javax.swing.JScrollPane;
-
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.swing.BoxLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import java.awt.BorderLayout;
 
 public class InterWin {
 
 	private JFrame frmMoea;
+	private JTextField txtNumVars;
+	private JTextField txtNumObjs;
+	private JTextField txtPopSize;
 	private JTextField txtNumGens;
-	private JTextArea textArea;
+	private JTextField txtPc;
+	private JTextField txtPm;
+	private JTextField txtBeta;
+	private JTextField txtEta;
 	/**
 	 * Launch the application.
 	 */
@@ -46,45 +59,116 @@ public class InterWin {
 	 */
 	private void initialize() {
 		frmMoea = new JFrame();
-		frmMoea.setResizable(false);
 		frmMoea.setTitle("MOEA");
 		frmMoea.setBounds(100, 100, 729, 415);
 		frmMoea.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmMoea.getContentPane().setLayout(null);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[] {420, 256, 0};
+		gridBagLayout.rowHeights = new int[] {63};
+		gridBagLayout.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0};
+		frmMoea.getContentPane().setLayout(gridBagLayout);
 		
-		JLabel lblThisIsA = new JLabel("# Generations");
-		lblThisIsA.setBounds(10, 11, 82, 23);
-		lblThisIsA.setHorizontalAlignment(SwingConstants.CENTER);
-		frmMoea.getContentPane().add(lblThisIsA);
-		
-		JButton btnRun = new JButton("Run");
-
-		btnRun.setBounds(624, 16, 89, 23);
-		frmMoea.getContentPane().add(btnRun);
-		
-		txtNumGens = new JTextField();
-		txtNumGens.setBounds(102, 12, 86, 20);
-		frmMoea.getContentPane().add(txtNumGens);
-		txtNumGens.setColumns(10);
+		JPanel panel = new JPanel();
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.anchor = GridBagConstraints.NORTH;
+		gbc_panel.insets = new Insets(0, 0, 5, 0);
+		gbc_panel.fill = GridBagConstraints.HORIZONTAL;
+		gbc_panel.gridx = 1;
+		gbc_panel.gridy = 0;
+		frmMoea.getContentPane().add(panel, gbc_panel);
 		
 		JButton btnStop = new JButton("Stop");
+		panel.add(btnStop);
 		btnStop.setBounds(525, 16, 89, 23);
-		frmMoea.getContentPane().add(btnStop);
 		
-		textArea = new JTextArea();
-		textArea.setBounds(20, 45, 680, 330);		
-		JScrollPane scrl = new JScrollPane(textArea);
-		scrl.setBounds(20, 45, 680, 330);
-		frmMoea.getContentPane().add(scrl);
-
+		JButton btnRun = new JButton("Run");
+		panel.add(btnRun);
 		
-		btnRun.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				for (int i = 0;i < 100;i++) {
-				textArea.setText(textArea.getText() + "i am using this kind of software to express my short-commings\n i need nastaran\n she is" +
-						"the only way i can set my self");
-				}
-			}
-		});
+				btnRun.setBounds(624, 16, 89, 23);
+				
+						
+						btnRun.addActionListener(new ActionListener() {
+							public void actionPerformed(ActionEvent arg0) {
+								for (int i = 0;i < 100;i++) {
+								}
+							}
+						});
+		
+		JPanel panelOptions = new JPanel();
+		panelOptions.setLayout(new GridLayout(10, 2, 0, 0));
+		
+		
+		JLabel lblNewLabel = new JLabel("# of Varialbes");
+		panelOptions.add(lblNewLabel);
+		
+		txtNumVars = new JTextField();
+		panelOptions.add(txtNumVars);
+		txtNumVars.setColumns(10);
+		
+		JLabel lblOfObjectives = new JLabel("# of Objectives");
+		panelOptions.add(lblOfObjectives);
+		
+		txtNumObjs = new JTextField();
+		panelOptions.add(txtNumObjs);
+		txtNumObjs.setColumns(10);
+		
+		JLabel lblPopulationSize = new JLabel("Population Size");
+		panelOptions.add(lblPopulationSize);
+		
+		txtPopSize = new JTextField();
+		panelOptions.add(txtPopSize);
+		txtPopSize.setColumns(10);
+		
+		JLabel lblOfGenerations = new JLabel("# of Generations");
+		panelOptions.add(lblOfGenerations);
+		
+		txtNumGens = new JTextField();
+		txtNumGens.setText("");
+		panelOptions.add(txtNumGens);
+		txtNumGens.setColumns(10);
+		
+		JLabel lblProblem = new JLabel("Problem");
+		panelOptions.add(lblProblem);
+		
+		JComboBox cmbProblem = new JComboBox();
+		panelOptions.add(cmbProblem);
+		
+		JLabel lblCrossoverRate = new JLabel("Crossover Rate");
+		panelOptions.add(lblCrossoverRate);
+		
+		txtPc = new JTextField();
+		txtPc.setText("");
+		panelOptions.add(txtPc);
+		txtPc.setColumns(10);
+		
+		JLabel lblMutationRate = new JLabel("Mutation Rate");
+		panelOptions.add(lblMutationRate);
+		
+		txtPm = new JTextField();
+		txtPm.setText("");
+		panelOptions.add(txtPm);
+		txtPm.setColumns(10);
+		
+		JLabel lblBeta = new JLabel("Beta");
+		panelOptions.add(lblBeta);
+		
+		txtBeta = new JTextField();
+		txtBeta.setText("");
+		panelOptions.add(txtBeta);
+		txtBeta.setColumns(10);
+		
+		JLabel lblEta = new JLabel("Eta");
+		panelOptions.add(lblEta);
+		
+		txtEta = new JTextField();
+		txtEta.setText("");
+		panelOptions.add(txtEta);
+		txtEta.setColumns(10);
+		GridBagConstraints gbc_panelOptions = new GridBagConstraints();
+		gbc_panelOptions.fill = GridBagConstraints.BOTH;
+		gbc_panelOptions.gridx = 1;
+		gbc_panelOptions.gridy = 1;
+		frmMoea.getContentPane().add(panelOptions, gbc_panelOptions);
 	}
 }
