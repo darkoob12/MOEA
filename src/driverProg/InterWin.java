f5e6cceed9a82aeef065d61e67f00c8f07698d63
Shahab Abbasi Rad <darkoob12@gmail.com>
2013-01-07 20:24:47 +0330

    i added a scrollable text field for logging algorithm progress

--- src/driverProg/InterWin.java
+++ src/driverProg/InterWin.java
@@ -4,13 +4,20 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import java.awt.BorderLayout;
 import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class InterWin {
 
-	private JFrame frame;
-
+	private JFrame frmMoea;
+	private JTextField txtNumGens;
+	private JTextArea textArea;
 	/**
 	 * Launch the application.
 	 */
@@ -19,7 +26,7 @@ public class InterWin {
 			public void run() {
 				try {
 					InterWin window = new InterWin();
-					window.frame.setVisible(true);
+					window.frmMoea.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -38,15 +45,46 @@ public class InterWin {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frmMoea = new JFrame();
+		frmMoea.setResizable(false);
+		frmMoea.setTitle("MOEA");
+		frmMoea.setBounds(100, 100, 729, 415);
+		frmMoea.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmMoea.getContentPane().setLayout(null);
 		
-		JLabel lblThisIsA = new JLabel("This is a Test");
-		lblThisIsA.setBounds(132, 58, 112, 33);
+		JLabel lblThisIsA = new JLabel("# Generations");
+		lblThisIsA.setBounds(10, 11, 82, 23);
 		lblThisIsA.setHorizontalAlignment(SwingConstants.CENTER);
-		frame.getContentPane().add(lblThisIsA);
-	}
+		frmMoea.getContentPane().add(lblThisIsA);
+		
+		JButton btnRun = new JButton("Run");
 
+		btnRun.setBounds(624, 16, 89, 23);
+		frmMoea.getContentPane().add(btnRun);
+		
+		txtNumGens = new JTextField();
+		txtNumGens.setBounds(102, 12, 86, 20);
+		frmMoea.getContentPane().add(txtNumGens);
+		txtNumGens.setColumns(10);
+		
+		JButton btnStop = new JButton("Stop");
+		btnStop.setBounds(525, 16, 89, 23);
+		frmMoea.getContentPane().add(btnStop);
+		
+		textArea = new JTextArea();
+		textArea.setBounds(20, 45, 680, 330);		
+		JScrollPane scrl = new JScrollPane(textArea);
+		scrl.setBounds(20, 45, 680, 330);
+		frmMoea.getContentPane().add(scrl);
+
+		
+		btnRun.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				for (int i = 0;i < 100;i++) {
+				textArea.setText(textArea.getText() + "i am using this kind of software to express my short-commings\n i need nastaran\n she is" +
+						"the only way i can set my self");
+				}
+			}
+		});
+	}
 }
